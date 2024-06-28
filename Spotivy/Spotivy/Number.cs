08c37d20b12e5af0d06c0373f10bea8c22b9bef3
Sintayu-de-Kuiper <sintayudekuiper@gmail.com>
2024-06-28 15:06:52 +0200

    Created a Number class and implemented a simple play function

---
+++ Spotivy/Spotivy/Number.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spotivy
+{
+    internal class Number(string songTitle, string artist)
+    {
+        public string Title { get; } = songTitle;
+        public string Artist { get; } = artist;
+
+        public void Play()
+        {
+            Console.WriteLine("Playing: " + Title + " by " + Artist);
+        }
+    }
+}
--- Spotivy/Spotivy/Program.cs
+++ Spotivy/Spotivy/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace Spotivy
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Spotivy
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Number song = new("OhYeah", "MJ");
+            Console.WriteLine(song.Title);
+            song.Play();
         }
     }
 }
